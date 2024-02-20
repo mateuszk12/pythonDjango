22270332d702b8530ff9ed8d01ae0db55f7d28d4
mati <mateusz.krz314@gmail.com>
2024-02-20 15:10:36 +0100

    created models

--- djangoProject/schedule/models.py
+++ djangoProject/schedule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
