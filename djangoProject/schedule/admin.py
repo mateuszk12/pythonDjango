37368cce886a25f6837e4385a1b93d3aa43b7d7f
mati <mateusz.krz314@gmail.com>
2024-02-20 15:39:18 +0100

    added migrations

--- djangoProject/schedule/admin.py
+++ djangoProject/schedule/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from schedule.models import Category
+from schedule.models import Task
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(Category)
