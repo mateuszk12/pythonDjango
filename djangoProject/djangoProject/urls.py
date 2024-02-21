859ea5b7ccfcc61d635afdb3d5a383e07daf800a
mati <mateusz.krz314@gmail.com>
2024-02-21 01:15:12 +0100

    changed urls

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schedule/',include("schedule.urls"))
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- djangoProject/schedule/urls.py
+++ djangoProject/schedule/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 import schedule.views
 
-
+app_name = "schedule"
 urlpatterns = [
     path("categories/",schedule.views.categories,name="categories"),
-    path("tasks/",schedule.views.tasks,name="tasks")
+    path("categories/<int:id>/",schedule.views.category,name="category"),
+    path("tasks/",schedule.views.tasks,name="tasks"),
+    path("tasks/<int:id>/",schedule.views.tasks,name="task")
 ]
